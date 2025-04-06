7657dedab642ee31db7aa4b14ee9dfb5ef7b0f41
Keaton Hoyle <keatonhoyle@gmail.com>
2025-04-06 16:00:51 -0400

    Configured static files for admin interface

--- whiteboard_project/settings.py
+++ whiteboard_project/settings.py
@@ -109,8 +109,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
