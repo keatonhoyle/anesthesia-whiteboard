d9b497382edfb642c0b559d239f5736264d6fb16
Keaton Hoyle <keatonhoyle@gmail.com>
2025-04-07 18:13:35 -0400

    Updated prompt-generator.py to save to prompt.txt and added Bootstrap to templates

--- whiteboard/urls.py
+++ whiteboard/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', views.custom_logout, name='logout'),
 ]
